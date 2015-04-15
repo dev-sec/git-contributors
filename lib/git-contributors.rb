e2a2a400987dc49482d8979fc8a094a3a5bc2098
Dominik Richter <dominik.richter@gmail.com>
2015-04-15 15:52:03 +0200

    improvement: structure repo+org handling into class
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- lib/git-contributors.rb
+++ lib/git-contributors.rb
@@ -12,32 +12,17 @@ class GitContributors
   FORMAT = '* %-20LOGIN   %-53NAME'
   PROVIDERS = RepoProviders.new
 
-  def initialize owner, repo
-    @user = owner
-    @repo = repo
-    @users = nil
+  attr_reader :repos, :users
+  def initialize
+    @repos = []
+    @users = {}
+    @github = RepoProvider::Github.new.provider
   end
 
-  def self.for_github_org org
-    github = RepoProvider::Github.new.provider
-    repos = github.org_repositories('hardening-io')
-
-    contributors = {}
-    contribs = repos.map do |repo|
-      GitContributors.new org, repo['name']
-    end.each do |contrib|
-      contrib.users.each do |u|
-        contributors[u['login']] ||= u
-        o = contributors[u['login']]
-        o['stats']['repos'] ||= 0
-        o['stats']['repos'] += 1
-        o['stats']['contributions'] += (u['stats']['contributions'] || 0)
-      end
-    end
-
-    gc = GitContributors.new nil, nil
-    gc.users = contributors
-    gc.print_contributors
+  def self.for_github_org user
+    gc = GitContributors.new
+    gc.add_org user
+    gc
   end
 
   def self.for_path path
@@ -46,36 +31,48 @@ class GitContributors
     Log.abort "no github repo here?" if prov.empty?
     user = prov[0].repo['user']
     repo = prov[0].repo['repo'].sub(/\.git/,'')
-    GitContributors.new user, repo
+    gc = GitContributors.new
+    gc.add_repo user, repo
+    gc
   end
 
-  def users
-    @users || load_contributors
-  end
-  def users= u
-    @users = u
+  def add_org user
+    repos = @github.org_repositories('hardening-io')
+    puts "Load #{repos.length} repos for #{user}"
+    repos.each do |repo|
+      add_repo user, repo['name']
+    end
   end
 
-  def load_contributors
-    github = RepoProvider::Github.new.provider
-    userlist = github.contributors("#{@user}/#{@repo}")
-
-    print "Load #{userlist.length} contributors for #{@user}/#{@repo}"
-    @users = userlist.map do |uinfo|
-      r = github.user(uinfo['login'])
-      r['stats'] = uinfo
-      print '.'
-      r
+  def add_repo user, repo
+    @repos.push({ user: user, repo: repo })
+    userlist = @github.contributors("#{user}/#{repo}")
+    # load all users
+    print "Load #{userlist.length} contributors for #{user}/#{repo} "
+    userlist.map do |uinfo|
+      uid = uinfo['login']
+      # progress indication
+      print (@users[uid].nil? ? '+' : '.')
+      # get the user, if we don't have his/her data yet
+      @users[uid] ||= @github.user(uid)
+      # get or set user stats
+      if @users[uid]['stats'].nil?
+        @users[uid]['stats'] = uinfo
+        @users[uid]['stats']['repos'] = 1
+      else
+        # we aggregate contributions and repo stats
+        @users[uid]['stats']['contributions'] += (uinfo['contributions'] || 0)
+        @users[uid]['stats']['repos'] += 1
+      end
     end
     print "\n"
-    @users
   end
 
   def print_contributors opts
     format = opts[:format] || FORMAT
 
     puts
-    users.each do |user|
+    users.each do |id, user|
       puts format_user_string(format, user)
     end
     puts
@@ -90,6 +87,7 @@ class GitContributors
       'avatar_url' => /(%[+-]?\d*)AVATAR/,
       'stats.contributions' => /(%[+-]?\d*)CONTRIBUTIONS/,
       'stats.repos' => /(%[+-]?\d*)REPOS/,
+      'html_url' => /(%[+-]?\d*)URL/,
     }
 
     # format array contains the position
