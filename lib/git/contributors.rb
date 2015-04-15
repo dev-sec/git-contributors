3610fc50c2f33d56daae1c09485277fb7033196d
Dominik Richter <dominik.richter@gmail.com>
2015-04-15 13:13:39 +0200

    initial
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

---
+++ lib/git/contributors.rb
@@ -0,0 +1,7 @@
+require "git/contributors/version"
+
+module Git
+  module Contributors
+    # Your code goes here...
+  end
+end
---
+++ lib/git/contributors/version.rb
@@ -0,0 +1,5 @@
+module Git
+  module Contributors
+    VERSION = "0.0.1"
+  end
+end
